41eb3a27b8eed5cf75d6904da6d9b8f8cff89e29
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-16 14:17:13 -0500

    board with initStart

--- SimpleBoard/java/simpleboard/SimpleBoard.java
+++ SimpleBoard/java/simpleboard/SimpleBoard.java
@@ -10,6 +10,12 @@ import base.board.Board;
  * This class is meant to be a simple implementation of Board -- it's not going to be the fastest
  */
 public class SimpleBoard implements Board {
+    private final int[] board = new int[100];
     public SimpleBoard() {
+        initStart();
+    }
+
+    private void initStart() {
+        
     }
 }
