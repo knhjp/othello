28bd86ef244777a292fd2870f1443af90c36a534
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-16 15:09:40 -0500

    added SimpleBoardTest

---
+++ SimpleBoard/test/simpleboard/SimpleBoardTest.java
@@ -0,0 +1,11 @@
+package simpleboard;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 16-Sep-2009
+ * Time: 3:08:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleBoardTest {
+}
