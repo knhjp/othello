7741eeda94e2157f9990b92a8a659fa6d3174db0
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-16 15:24:48 -0500

    added OthelloTestCase

---
+++ Base/java/base/testcase/OthelloTestCase.java
@@ -0,0 +1,11 @@
+package base.testcase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 16-Sep-2009
+ * Time: 3:24:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OthelloTestCase {
+}
