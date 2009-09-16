6f3a333909096038aa8583bfed525f0cd8dbae4c
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-16 15:25:54 -0500

    A description for OthelloTestCase

--- Base/java/base/testcase/OthelloTestCase.java
+++ Base/java/base/testcase/OthelloTestCase.java
@@ -7,7 +7,8 @@ import junit.framework.TestCase;
  * User: knhjp
  * Date: 16-Sep-2009
  * Time: 3:24:26 PM
- * To change this template use File | Settings | File Templates.
+ * OthelloTestCase is meant to be the root of all the test cases. This makes it easy for me to add something that
+ * applies to all of my test cases
  */
 public class OthelloTestCase extends TestCase {
 }
