f7dcf1f15a7d035b6b28bbe06e0f4718e35a092a
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-16 18:15:32 -0500

    Added the interface -- TreeSearch

---
+++ Search/java/search/TreeSearch.java
@@ -0,0 +1,11 @@
+package search;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 16-Sep-2009
+ * Time: 6:15:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TreeSearch {
+}
