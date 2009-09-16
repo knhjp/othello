33c84cd58a3e69818b82e8c7f934e7a5ac81bd3e
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-16 18:12:34 -0500

    Created class MiniMax

---
+++ Search/java/search/minimax/MiniMax.java
@@ -0,0 +1,11 @@
+package search.minimax;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 16-Sep-2009
+ * Time: 6:12:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MiniMax {
+}
