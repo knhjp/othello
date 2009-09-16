7291ca00485c7542b5805a3486a751c19a3a7263
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-16 10:50:05 -0500

    Intorudcing Evalator, which is something that gives you a score given a position

---
+++ Base/java/base/eval/Evaluator.java
@@ -0,0 +1,11 @@
+package base.eval;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 16-Sep-2009
+ * Time: 10:46:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Evaluator {
+}
