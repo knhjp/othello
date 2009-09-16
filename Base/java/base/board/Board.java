e3b721ebe47276337da316c3b7463cb6cc9302e8
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-16 10:45:18 -0500

    introducing interface -- Board

---
+++ Base/java/base/board/Board.java
@@ -0,0 +1,11 @@
+package base.board;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 16-Sep-2009
+ * Time: 10:29:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Board {
+}
