9cf4a7a0ad72c35bb6bd24ee895376ecbd04c6b5
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-19 13:04:56 -0500

    added description

--- SimpleBoard/test/simpleboard/BoardForTesting.java
+++ SimpleBoard/test/simpleboard/BoardForTesting.java
@@ -5,7 +5,7 @@ package simpleboard;
  * User: knhjp
  * Date: 19-Sep-2009
  * Time: 1:04:01 PM
- * To change this template use File | Settings | File Templates.
+ * This board allows us to set the position
  */
 public class BoardForTesting{
 }
