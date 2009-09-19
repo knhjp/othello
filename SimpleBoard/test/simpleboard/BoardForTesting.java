1b22cfd048b9ba0a708f65fc385d292fccc56cd6
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-19 13:04:22 -0500

    introducing BoardForTesting

---
+++ SimpleBoard/test/simpleboard/BoardForTesting.java
@@ -0,0 +1,11 @@
+package simpleboard;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 19-Sep-2009
+ * Time: 1:04:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BoardForTesting{
+}
