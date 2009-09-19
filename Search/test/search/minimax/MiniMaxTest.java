1cae25072c7e5fc37ee4612611f77d89db3ef23c
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-19 13:03:05 -0500

    added MiniMaxTest

---
+++ Search/test/search/minimax/MiniMaxTest.java
@@ -0,0 +1,11 @@
+package search.minimax;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 19-Sep-2009
+ * Time: 1:02:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MiniMaxTest {
+}
