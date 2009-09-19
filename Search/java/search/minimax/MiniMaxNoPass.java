659e80c4043cf39fd2e84152eddd373156431021
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-19 12:39:58 -0500

    Make MiniMaxNoPass extend AbstractMiniMax

--- Search/java/search/minimax/MiniMaxNoPass.java
+++ Search/java/search/minimax/MiniMaxNoPass.java
@@ -1,5 +1,7 @@
 package search.minimax;
 
+import base.board.Board;
+
 /**
  * Created by IntelliJ IDEA.
  * User: knhjp
@@ -7,5 +9,9 @@ package search.minimax;
  * Time: 12:37:51 PM
  * This class handles MiniMax search when there has been no passes made yet
  */
-public class MiniMaxNoPass {
+public class MiniMaxNoPass extends AbstractMiniMax {
+
+    @Override int handlePass(int color, Board[] board, int empties) {
+        return 0;
+    }
 }
