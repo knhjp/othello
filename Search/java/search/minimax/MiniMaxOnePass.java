00f2c5a8d97ab10f9a4efbdda16ae9f7f3dcacf0
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-19 12:42:26 -0500

    added description

--- Search/java/search/minimax/MiniMaxOnePass.java
+++ Search/java/search/minimax/MiniMaxOnePass.java
@@ -5,7 +5,7 @@ package search.minimax;
  * User: knhjp
  * Date: 19-Sep-2009
  * Time: 12:40:40 PM
- * To change this template use File | Settings | File Templates.
+ * This class handles the Minimax search when there is already one pass
  */
 public class MiniMaxOnePass {
 }
