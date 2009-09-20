f3b702f41c9d6f2ff6a1837f8a7ddb14c7119cf1
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-20 18:32:41 -0500

    introducing SetupBoardTest

---
+++ SimpleBoard/test/simpleboard/SetupBoardTest.java
@@ -0,0 +1,11 @@
+package simpleboard;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 20, 2009
+ * Time: 6:32:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SetupBoardTest {
+}
