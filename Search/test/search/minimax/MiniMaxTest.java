488a6b05c0478f92c09b34a2c8f74ad96694f093
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-20 21:19:19 -0500

    added dependency to SimpleBoard (using it to test minimax)

--- Search/test/search/minimax/MiniMaxTest.java
+++ Search/test/search/minimax/MiniMaxTest.java
@@ -1,6 +1,7 @@
 package search.minimax;
 
 import base.testcase.OthelloTestCase;
+import simpleboard.SetupBoard;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,4 +11,23 @@ import base.testcase.OthelloTestCase;
  * This class tests that we can do some simple search
  */
 public class MiniMaxTest extends OthelloTestCase {
+    public void testSimpleCase() {
+        int[] myBoard = {
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, -1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, -1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+
+        SetupBoard board = new SetupBoard();
+        board.resetToStart();
+        board.setBoard(myBoard);
+
+        
+    }
 }
