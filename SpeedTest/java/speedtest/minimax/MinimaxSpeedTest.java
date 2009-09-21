0244269c22f4bba8c11c6cade537f9c6e4b40b3a
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-21 18:10:53 -0500

    Introducing MinimaxSpeedTest

---
+++ SpeedTest/java/speedtest/minimax/MinimaxSpeedTest.java
@@ -0,0 +1,11 @@
+package speedtest.minimax;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 21, 2009
+ * Time: 6:10:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MinimaxSpeedTest {
+}
