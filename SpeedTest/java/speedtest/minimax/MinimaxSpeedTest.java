e9aa3453cd127f688b29de77db3349d3dbd39aee
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-21 18:32:41 -0500

    code for actually doing a solve

--- SpeedTest/java/speedtest/minimax/MinimaxSpeedTest.java
+++ SpeedTest/java/speedtest/minimax/MinimaxSpeedTest.java
@@ -1,5 +1,9 @@
 package speedtest.minimax;
 
+import search.minimax.MiniMax;
+import simpleboard.SetupBoard;
+import base.board.Board;
+
 /**
  * Created by IntelliJ IDEA.
  * User: knhjp
@@ -9,6 +13,33 @@ package speedtest.minimax;
  */
 public class MinimaxSpeedTest {
     public static void main(String[] args) {
-        
+        MiniMax miniMax = new MiniMax();
+
+        int[] myBoard = {
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, -1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, -1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+
+        SetupBoard board = new SetupBoard();
+        board.resetToStart();
+        board.setBoard(myBoard);
+
+        int color = 1;
+        int empties = 18;
+        boolean alreadyPassed = false;
+        Board[] boards = new Board[60];
+        for (int i=0 ; i<boards.length ; i++) {
+            boards[i] = new SetupBoard();
+        }
+        boards[empties] = board;
+
+        miniMax.search(color,boards,empties,alreadyPassed);
     }
 }
