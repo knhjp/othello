5ec5b7ec4c5b1663b4581e0d07563ee79c9f10c2
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-21 19:42:03 -0500

    added a test with an early end

--- Search/test/search/alphabeta/AlphaBetaTest.java
+++ Search/test/search/alphabeta/AlphaBetaTest.java
@@ -40,4 +40,33 @@ public class AlphaBetaTest extends OthelloTestCase {
         int score = alphaBeta.search(1, boards, 3, false,-65,65);
         assertEquals(64,score);
     }
+
+    public void testWithEarlyEnd() {
+        int[] myBoard = {
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, -1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+
+        SetupBoard board = new SetupBoard();
+        board.resetToStart();
+        board.setBoard(myBoard);
+
+        AlphaBeta alphaBeta = new AlphaBeta();
+
+        Board[] boards = new Board[60];
+        for (int i=0 ; i<boards.length ; i++) {
+            boards[i] = new SetupBoard();
+        }
+
+        boards[3] = board;
+        int score = alphaBeta.search(1, boards, 3, false,-65,65);
+        assertEquals(63,score);
+    }
 }
