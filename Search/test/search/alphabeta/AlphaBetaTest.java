02e2155fccb4b5c4d4e01e3231170849341fda6f
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-21 19:41:06 -0500

    test for simple case

--- Search/test/search/alphabeta/AlphaBetaTest.java
+++ Search/test/search/alphabeta/AlphaBetaTest.java
@@ -1,6 +1,8 @@
 package search.alphabeta;
 
 import base.testcase.OthelloTestCase;
+import base.board.Board;
+import simpleboard.SetupBoard;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,4 +12,32 @@ import base.testcase.OthelloTestCase;
  * This is a unit test for testing AlphaBeta
  */
 public class AlphaBetaTest extends OthelloTestCase {
+    public void testSimpleCase() {
+        int[] myBoard = {
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 0, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, -1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, -1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+
+        SetupBoard board = new SetupBoard();
+        board.resetToStart();
+        board.setBoard(myBoard);
+
+        AlphaBeta alphaBeta = new AlphaBeta();
+
+        Board[] boards = new Board[60];
+        for (int i=0 ; i<boards.length ; i++) {
+            boards[i] = new SetupBoard();
+        }
+
+        boards[3] = board;
+        int score = alphaBeta.search(1, boards, 3, false,-65,65);
+        assertEquals(64,score);
+    }
 }
