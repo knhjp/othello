0309d21bce15e5dcd90d8f284ba58b78cb785aad
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-21 19:39:36 -0500

    Introducing AlphaBetaTest

---
+++ Search/test/search/alphabeta/AlphaBetaTest.java
@@ -0,0 +1,11 @@
+package search.alphabeta;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 21, 2009
+ * Time: 7:39:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlphaBetaTest {
+}
