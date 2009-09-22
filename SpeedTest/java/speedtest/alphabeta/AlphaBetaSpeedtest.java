91b921c0c06d055cdb472d832b69cdf1a1c60f1f
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-21 19:44:25 -0500

    added AlphaBetaSpeedtest

---
+++ SpeedTest/java/speedtest/alphabeta/AlphaBetaSpeedtest.java
@@ -0,0 +1,11 @@
+package speedtest.alphabeta;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 21, 2009
+ * Time: 7:44:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlphaBetaSpeedtest {
+}
