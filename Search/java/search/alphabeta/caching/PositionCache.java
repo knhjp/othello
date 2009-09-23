6df6181d188b3b4440ae044df21da18100c6375e
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-22 21:33:48 -0500

    Introducing PositionCache

---
+++ Search/java/search/alphabeta/caching/PositionCache.java
@@ -0,0 +1,11 @@
+package search.alphabeta.caching;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 22, 2009
+ * Time: 9:33:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PositionCache {
+}
