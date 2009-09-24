f012464833a7cd1460ab20bc6ddf02612fe8d41c
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-23 21:10:43 -0500

    Introducing AlphaBetaCachingTest

---
+++ Search/test/search/alphabeta/caching/AlphaBetaCachingTest.java
@@ -0,0 +1,11 @@
+package search.alphabeta.caching;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 23, 2009
+ * Time: 9:10:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlphaBetaCachingTest {
+}
