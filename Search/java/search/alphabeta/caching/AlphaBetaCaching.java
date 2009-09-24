d3b086f95783bbdb7c330c3f428bb04a8a0d8615
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-23 20:02:20 -0500

    copied AlphaBeta's code over... before we start mucking with it

--- Search/java/search/alphabeta/caching/AlphaBetaCaching.java
+++ Search/java/search/alphabeta/caching/AlphaBetaCaching.java
@@ -1,5 +1,7 @@
 package search.alphabeta.caching;
 
+import base.board.Board;
+
 /**
  * Created by IntelliJ IDEA.
  * User: knhjp
@@ -8,4 +10,38 @@ package search.alphabeta.caching;
  * This aims to do the same thing as AlphaBeta, except it caches positions already seen
  */
 public class AlphaBetaCaching {
+    private int nodeCount;
+    public AlphaBetaCaching() {
+        nodeCount=0;
+    }
+
+    public int search(int color, Board[] boards, int empties, boolean alreadyPassed, int alpha, int beta) {
+        nodeCount++;
+        boolean hasSearched = false;
+        for (int curLocation : Board.allMoves) {
+            if (boards[empties].isMoveValid(color,curLocation)) {
+                hasSearched = true;
+                boards[empties-1].copyBoard(boards[empties]);
+                boards[empties-1].makeMove(color,curLocation);
+                alpha = Math.max(alpha,-search(-color,boards,empties-1,false,-beta,-alpha));
+                if (beta <= alpha) {//beta cutoff
+                    return alpha;
+                }
+            }
+        }
+
+        if (hasSearched) {
+            return alpha;
+        } else { //this happens in case of a pass
+            if (alreadyPassed) {
+                return boards[empties].getBlackMinusWhite() * color;
+            } else {
+                return -search(-color,boards,empties,true,-beta,-alpha);
+            }
+        }
+    }
+
+    public int getNodeCount() {
+        return nodeCount;
+    }
 }
