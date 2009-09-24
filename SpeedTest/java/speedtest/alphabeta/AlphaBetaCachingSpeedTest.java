d0808e2dfae071ae7f32c708e35268668cf26359
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-24 18:35:57 -0500

    Created AlphaBetaCachingSpeedTest for testing the new code

---
+++ SpeedTest/java/speedtest/alphabeta/AlphaBetaCachingSpeedTest.java
@@ -0,0 +1,11 @@
+package speedtest.alphabeta;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 24, 2009
+ * Time: 6:35:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlphaBetaCachingSpeedTest {
+}
