4876ade8bf8294a5abdbea42fc8d2ba3f8944bc1
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-23 20:01:45 -0500

    added a description

--- Search/java/search/alphabeta/caching/AlphaBetaCaching.java
+++ Search/java/search/alphabeta/caching/AlphaBetaCaching.java
@@ -5,7 +5,7 @@ package search.alphabeta.caching;
  * User: knhjp
  * Date: Sep 23, 2009
  * Time: 8:01:05 PM
- * To change this template use File | Settings | File Templates.
+ * This aims to do the same thing as AlphaBeta, except it caches positions already seen
  */
 public class AlphaBetaCaching {
 }
