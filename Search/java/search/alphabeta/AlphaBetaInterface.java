f17357dd95d2947e5b56a050c929d3b594fdc078
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-24 21:05:47 -0500

    Introducing AlphaBetaInterface

---
+++ Search/java/search/alphabeta/AlphaBetaInterface.java
@@ -0,0 +1,11 @@
+package search.alphabeta;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 24, 2009
+ * Time: 9:05:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AlphaBetaInterface {
+}
