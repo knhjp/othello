e86340c700656dc98d718c4303aa187c7ee11764
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-24 21:06:20 -0500

    added description for the interface

--- Search/java/search/alphabeta/AlphaBetaInterface.java
+++ Search/java/search/alphabeta/AlphaBetaInterface.java
@@ -5,7 +5,7 @@ package search.alphabeta;
  * User: knhjp
  * Date: Sep 24, 2009
  * Time: 9:05:35 PM
- * To change this template use File | Settings | File Templates.
+ * This makes a consistent interface to alpha-beta search
  */
 public interface AlphaBetaInterface {
 }
