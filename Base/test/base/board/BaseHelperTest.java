626ef5f1e15f58d5cfda9882b20357b86437127e
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 19:41:21 -0500

    Introducing BaseHelperTest

---
+++ Base/test/base/board/BaseHelperTest.java
@@ -0,0 +1,11 @@
+package base.board;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 25, 2009
+ * Time: 7:41:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseHelperTest {
+}
