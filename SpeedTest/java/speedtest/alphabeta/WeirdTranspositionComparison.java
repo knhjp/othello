fdc189e81cfeadf35061243efaf6305c574ec187
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 20:07:26 -0500

    added WeirdTranpositionsComparison

---
+++ SpeedTest/java/speedtest/alphabeta/WeirdTranspositionComparison.java
@@ -0,0 +1,11 @@
+package speedtest.alphabeta;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 25, 2009
+ * Time: 8:07:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WeirdTranspositionComparison {
+}
