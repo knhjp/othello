dfd737e7d486b4c8879d58ef82051ebbf6bda64f
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 20:37:22 -0500

    Introducing FastBoard

---
+++ FastBoard/java/fastboard/FastBoard.java
@@ -0,0 +1,11 @@
+package fastboard;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 25, 2009
+ * Time: 8:37:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastBoard {
+}
