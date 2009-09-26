30ce4ef54443fe003c19bdaabffd044a0d0f78e0
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 20:01:08 -0500

    attempted to fill in data

--- Base/java/base/board/BoardHelper.java
+++ Base/java/base/board/BoardHelper.java
@@ -11,7 +11,21 @@ public class BoardHelper {
     public static int[] stringToMoves(String sequence) {
         int[] temp = new int[60];
         int counter = 0;
-        
+
+        String lowerCase = sequence.toLowerCase();
+
+        for (int i=0; i<lowerCase.length()-1 ; i++) {
+            char curChar = lowerCase.charAt(i);
+            int diff = curChar - 'a';
+            if (diff>=0 && diff<8) {
+                char nextChar = lowerCase.charAt(i + 1);
+                int nextDiff = nextChar - '1';
+                if (nextDiff>=0 && nextDiff<8) {
+                    temp[counter] = (diff+1) * 10 + nextDiff +1;
+                    i++;
+                }
+            }
+        }
 
         int[] ret = new int[counter];
         System.arraycopy(temp,0,ret,0,counter);
