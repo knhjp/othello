61af3fefc84e1172f3a97bd0488114e28191421c
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-26 11:38:22 -0500

    added description for FastBoardFlips

--- FastBoard/java/fastboard/FastBoardFlips.java
+++ FastBoard/java/fastboard/FastBoardFlips.java
@@ -5,7 +5,7 @@ package fastboard;
  * User: knhjp
  * Date: 26-Sep-2009
  * Time: 11:37:03 AM
- * To change this template use File | Settings | File Templates.
+ * This class handles all the flipping/placing of squares
  */
 public class FastBoardFlips {
 }
