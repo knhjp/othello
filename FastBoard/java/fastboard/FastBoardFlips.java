a6fe53d75e1540419e497f736b5d42df1494f154
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-26 11:39:07 -0500

    moved code to FastBoardFlips

--- FastBoard/java/fastboard/FastBoardFlips.java
+++ FastBoard/java/fastboard/FastBoardFlips.java
@@ -7,5 +7,28 @@ package fastboard;
  * Time: 11:37:03 AM
  * This class handles all the flipping/placing of squares
  */
-public class FastBoardFlips {
+public class FastBoardFlips extends FastBoardLines {
+    public void blackPlaceH8() {
+        a8_h8+=1;
+        a1_h8+=1;
+        h1_h8+=1;
+    }
+
+    public void blackFlipH8() {
+        a8_h8-=1;
+        a1_h8-=1;
+        h1_h8-=1;
+    }
+
+    public void whitePlaceH8() {
+        a8_h8+=2;
+        a1_h8+=2;
+        h1_h8+=2;
+    }
+
+    public void whiteFlipH8() {
+        a8_h8+=1;
+        a1_h8+=1;
+        h1_h8+=1;
+    }
 }
--- FastBoard/java/fastboard/generated/FastBoardMoves.java
+++ FastBoard/java/fastboard/generated/FastBoardMoves.java
@@ -10,28 +10,5 @@ import fastboard.FastBoardLines;
  * This is an auto-generated class! Please edit FastBoardMovesGenerator instead if you want to make changes.
  */
 public class FastBoardMoves extends FastBoardLines{
-
-    public void blackPlaceH8() {
-        a8_h8+=1;
-        a1_h8+=1;
-        h1_h8+=1;
-    }
-
-    public void blackFlipH8() {
-        a8_h8-=1;
-        a1_h8-=1;
-        h1_h8-=1;
-    }
-
-    public void whitePlaceH8() {
-        a8_h8+=2;
-        a1_h8+=2;
-        h1_h8+=2;
-    }
-
-    public void whiteFlipH8() {
-        a8_h8+=1;
-        a1_h8+=1;
-        h1_h8+=1;
-    }
+    
 }
