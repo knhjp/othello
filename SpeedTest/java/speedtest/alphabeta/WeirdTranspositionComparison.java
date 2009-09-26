9e3eaedd3a9936b59c2c84611cc5ca347fc407de
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 20:08:13 -0500

    added description

--- SpeedTest/java/speedtest/alphabeta/WeirdTranspositionComparison.java
+++ SpeedTest/java/speedtest/alphabeta/WeirdTranspositionComparison.java
@@ -5,7 +5,12 @@ package speedtest.alphabeta;
  * User: knhjp
  * Date: Sep 25, 2009
  * Time: 8:07:14 PM
- * To change this template use File | Settings | File Templates.
+ * This tests weird tranposition game:
+ * F5D6C3D3C4F4C5B3C2E6C6B6B4F6G3B5A3E3D2F3G4E2A4D1F1B1E1F2C7A5G5C8G6H3D7H4A6H5H2H1G1B7C1B2A2A1A8A7G2F7E7E8
+
+ * Then D8 Pass H7 H6 (Black to Move) or H7 H6 D8 (White To Move) with
+ * same position but different color to play - good test for your
+  * transposition table ? :)
  */
 public class WeirdTranspositionComparison {
 }
