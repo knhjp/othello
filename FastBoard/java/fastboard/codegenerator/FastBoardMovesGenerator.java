723341d147e1e267e147a08617d397362da465c9
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 22:09:24 -0500

    Introducing FastBoardMovesGenerator

---
+++ FastBoard/java/fastboard/codegenerator/FastBoardMovesGenerator.java
@@ -0,0 +1,11 @@
+package fastboard.codegenerator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 25, 2009
+ * Time: 10:09:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastBoardMovesGenerator {
+}
