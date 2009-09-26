42a249b6bc0729025c144f59cc5354db0b9fd817
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 22:20:51 -0500

    moved the lines over to FastBoardLines

--- FastBoard/java/fastboard/FastBoard.java
+++ FastBoard/java/fastboard/FastBoard.java
@@ -8,49 +8,4 @@ package fastboard;
  * This is meant to be a fast implementation of Board
  */
 public class FastBoard {
-    //horizonal
-    private int a1_h1;
-    private int a2_h2;
-    private int a3_h3;
-    private int a4_h4;
-    private int a5_h5;
-    private int a6_h6;
-    private int a7_h7;
-    private int a8_h8;
-
-    //vertical
-    private int a1_a8;
-    private int b1_b8;
-    private int c1_c8;
-    private int d1_d8;
-    private int e1_e8;
-    private int f1_f8;
-    private int g1_g8;
-    private int h1_h8;
-
-    // diagonal /
-    private int a3_c1;
-    private int a4_d1;
-    private int a5_e1;
-    private int a6_f1;
-    private int a7_g1;
-    private int a8_h1;
-    private int b8_h2;
-    private int c8_h3;
-    private int d8_h4;
-    private int e8_h5;
-    private int f8_h6;
-
-    //diagonal \
-    private int f1_h3;
-    private int e1_h4;
-    private int d1_h5;
-    private int c1_h6;
-    private int b1_h7;
-    private int a1_h8;
-    private int a2_g8;
-    private int a3_f8;
-    private int a4_e8;
-    private int a5_d8;
-    private int a6_c8;
 }
--- FastBoard/java/fastboard/FastBoardLines.java
+++ FastBoard/java/fastboard/FastBoardLines.java
@@ -8,4 +8,49 @@ package fastboard;
  * This just holds all the objects for the auto-generated code to work on
  */
 public class FastBoardLines {
+    //horizonal
+    protected int a1_h1;
+    protected int a2_h2;
+    protected int a3_h3;
+    protected int a4_h4;
+    protected int a5_h5;
+    protected int a6_h6;
+    protected int a7_h7;
+    protected int a8_h8;
+
+    //vertical
+    protected int a1_a8;
+    protected int b1_b8;
+    protected int c1_c8;
+    protected int d1_d8;
+    protected int e1_e8;
+    protected int f1_f8;
+    protected int g1_g8;
+    protected int h1_h8;
+
+    // diagonal /
+    protected int a3_c1;
+    protected int a4_d1;
+    protected int a5_e1;
+    protected int a6_f1;
+    protected int a7_g1;
+    protected int a8_h1;
+    protected int b8_h2;
+    protected int c8_h3;
+    protected int d8_h4;
+    protected int e8_h5;
+    protected int f8_h6;
+
+    //diagonal \
+    protected int f1_h3;
+    protected int e1_h4;
+    protected int d1_h5;
+    protected int c1_h6;
+    protected int b1_h7;
+    protected int a1_h8;
+    protected int a2_g8;
+    protected int a3_f8;
+    protected int a4_e8;
+    protected int a5_d8;
+    protected int a6_c8;
 }
