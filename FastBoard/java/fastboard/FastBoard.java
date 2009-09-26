21dac2a3baeb3fa3cd6004d75b98d241b420d7cb
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 21:26:52 -0500

    ints for keeping track of the lines

--- FastBoard/java/fastboard/FastBoard.java
+++ FastBoard/java/fastboard/FastBoard.java
@@ -8,4 +8,14 @@ package fastboard;
  * This is meant to be a fast implementation of Board
  */
 public class FastBoard {
+    //horizonal
+    private int a1_h1;
+    private int a2_h2;
+    private int a3_h3;
+    private int a4_h4;
+    private int a5_h5;
+    private int a6_h6;
+    private int a7_h7;
+    private int a8_h8;
+
 }
