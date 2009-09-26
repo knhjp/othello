6d1db44ea8713a4cadf847bca95f4bd08479eaea
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 22:10:33 -0500

    Added FastBoardMoves -- this will be automatically generated later on

---
+++ FastBoard/java/fastboard/generated/FastBoardMoves.java
@@ -0,0 +1,11 @@
+package fastboard.generated;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Sep 25, 2009
+ * Time: 10:10:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastBoardMoves {
+}
