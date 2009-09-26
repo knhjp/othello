c86e3155a8025163672ebffb166779537f67477d
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-09-25 21:31:44 -0500

    added the diagonals

--- FastBoard/java/fastboard/FastBoard.java
+++ FastBoard/java/fastboard/FastBoard.java
@@ -28,4 +28,29 @@ public class FastBoard {
     private int g1_g8;
     private int h1_h8;
 
+    // diagonal /
+    private int a3_c1;
+    private int a4_d1;
+    private int a5_e1;
+    private int a6_f1;
+    private int a7_g1;
+    private int a8_h1;
+    private int b8_h2;
+    private int c8_h3;
+    private int d8_h4;
+    private int e8_h5;
+    private int f8_h6;
+
+    //diagonal \
+    private int f1_h3;
+    private int e1_h4;
+    private int d1_h5;
+    private int c1_h6;
+    private int b1_h7;
+    private int a1_h8;
+    private int a2_g8;
+    private int a3_f8;
+    private int a4_e8;
+    private int a5_d8;
+    private int a6_c8;
 }
