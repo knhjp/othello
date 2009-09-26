09634702995ce994970ceeee6fab56ff3b17d890
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-26 11:37:21 -0500

    Introducing FastBoardFlips

---
+++ FastBoard/java/fastboard/FastBoardFlips.java
@@ -0,0 +1,11 @@
+package fastboard;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 26-Sep-2009
+ * Time: 11:37:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastBoardFlips {
+}
