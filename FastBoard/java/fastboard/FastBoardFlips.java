6b980fb588d568a877c26d8846d0f6e971455b82
adsfadsfhs <adsfadsfhs@gmail.com>
2009-09-26 11:45:12 -0500

    Added code for a1

--- FastBoard/java/fastboard/FastBoardFlips.java
+++ FastBoard/java/fastboard/FastBoardFlips.java
@@ -8,6 +8,30 @@ package fastboard;
  * This class handles all the flipping/placing of squares
  */
 public class FastBoardFlips extends FastBoardLines {
+    //a1
+    public void blackPlaceA1() {
+        a1_h1+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+        a1_h8+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+        a1_a8+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+    }
+
+    public void blackFlipA1() {
+        a1_h1-=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+        a1_h8-=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+        a1_a8-=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+    }
+
+    public void whitePlaceA1() {
+        a1_h1+=2 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+        a1_h8+=2 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+        a1_a8+=2 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+    }
+
+    public void whiteFlipA1() {
+        a1_h1+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+        a1_h8+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+        a1_a8+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+    }
 
     //h8
     public void blackPlaceH8() {
