e36f311fe21fd4b5e25b3814b718e38c28f46535
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-08 11:43:25 -0600

    updated description

--- FastBoard/java/fastboard/checkmove/helper/FastFlipCalcHelper.java
+++ FastBoard/java/fastboard/checkmove/helper/FastFlipCalcHelper.java
@@ -5,7 +5,8 @@ package fastboard.checkmove.helper;
  * User: knhjp
  * Date: Nov 7, 2009
  * Time: 7:58:23 PM
- * This helper class it used to determine whethre or not a disc is of a particular color
+ * This helper class it used to determine whether or not a disc is of a particular color
  */
 public interface FastFlipCalcHelper {
+
 }
--- FastBoard/java/fastboard/checkmove/helper/FastFlipCalcHelper0.java
+++ FastBoard/java/fastboard/checkmove/helper/FastFlipCalcHelper0.java
@@ -5,7 +5,7 @@ package fastboard.checkmove.helper;
  * User: knhjp
  * Date: Nov 8, 2009
  * Time: 11:37:10 AM
- * This class is used to determine whether or not a move is valid on the "square 0"
+ * This class is used to determine the color of square 0
  */
 public class FastFlipCalcHelper0 implements FastFlipCalcHelper {
     
