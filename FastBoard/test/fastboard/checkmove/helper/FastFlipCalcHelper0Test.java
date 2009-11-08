631633e9404cb066e2559907d49d5860aa66ebb3
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-08 11:51:29 -0600

    added FastFlipCalcHelper0Test

---
+++ FastBoard/test/fastboard/checkmove/helper/FastFlipCalcHelper0Test.java
@@ -0,0 +1,11 @@
+package fastboard.checkmove.helper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Nov 8, 2009
+ * Time: 11:51:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastFlipCalcHelper0Test {
+}
