c2bce8a91481c743aade7e65c26f8d4033006b07
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-08 11:40:55 -0600

    implements the interface

--- FastBoard/java/fastboard/checkmove/helper/FastFlipCalcHelper0.java
+++ FastBoard/java/fastboard/checkmove/helper/FastFlipCalcHelper0.java
@@ -7,5 +7,6 @@ package fastboard.checkmove.helper;
  * Time: 11:37:10 AM
  * This class is used to determine whether or not a move is valid on the "square 0"
  */
-public class FastFlipCalcHelper0 {
+public class FastFlipCalcHelper0 implements FastFlipCalcHelper {
+    
 }
