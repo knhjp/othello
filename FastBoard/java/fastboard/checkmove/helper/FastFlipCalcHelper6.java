518376eb7bc56202cf16111de0d8f4d1ee9cbdc3
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-08 16:33:03 -0600

    added helper 6

---
+++ FastBoard/java/fastboard/checkmove/helper/FastFlipCalcHelper6.java
@@ -0,0 +1,22 @@
+package fastboard.checkmove.helper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Nov 8, 2009
+ * Time: 11:37:10 AM
+ * This class is used to determine the color of square 6
+ */
+public class FastFlipCalcHelper6 implements FastFlipCalcHelper {
+    @Override public boolean isWhite(int line) {
+        return (line/(3*3*3*3*3*3))%3 == 2;
+    }
+
+    @Override public boolean isBlack(int line) {
+        return (line/(3*3*3*3*3*3))%3 == 1;
+    }
+
+    @Override public boolean isEmpty(int line) {
+        return (line/(3*3*3*3*3*3))%3 == 0;
+    }
+}
