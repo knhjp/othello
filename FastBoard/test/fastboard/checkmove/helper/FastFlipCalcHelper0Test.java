df2d5c0465e9227335fff71f4e96817631daf6b0
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-08 15:59:05 -0600

    make FastFlipCalcHelper0Test extends OthelloTestCase

--- FastBoard/test/fastboard/checkmove/helper/FastFlipCalcHelper0Test.java
+++ FastBoard/test/fastboard/checkmove/helper/FastFlipCalcHelper0Test.java
@@ -1,5 +1,7 @@
 package fastboard.checkmove.helper;
 
+import base.testcase.OthelloTestCase;
+
 /**
  * Created by IntelliJ IDEA.
  * User: knhjp
@@ -7,5 +9,5 @@ package fastboard.checkmove.helper;
  * Time: 11:51:14 AM
  * This class tests and make sure FastFlipCalcHelper correctly determines whether or not a particular disc is white, black, or empty 
  */
-public class FastFlipCalcHelper0Test {
+public class FastFlipCalcHelper0Test extends OthelloTestCase {
 }
