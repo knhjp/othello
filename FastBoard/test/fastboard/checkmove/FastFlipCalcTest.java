4f1c74c223980cab4d6ebd8cfb16713465a2e5ef
adsfadsfhs <adsfadsfhs@gmail.com>
2009-11-10 15:11:43 -0600

    Created FastFlipCalcTest

---
+++ FastBoard/test/fastboard/checkmove/FastFlipCalcTest.java
@@ -0,0 +1,11 @@
+package fastboard.checkmove;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 10-Nov-2009
+ * Time: 3:09:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastFlipCalcTest {
+}
