e9f25fbaf5146449d7decba64182c3d65fb302a3
adsfadsfhs <adsfadsfhs@gmail.com>
2009-11-10 16:51:55 -0600

    introducing line converter

---
+++ FastBoard/java/fastboard/lineconverter/LineConverter.java
@@ -0,0 +1,11 @@
+package fastboard.lineconverter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 10-Nov-2009
+ * Time: 4:51:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LineConverter {
+}
