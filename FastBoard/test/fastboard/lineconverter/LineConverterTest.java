4e343d243e40ef492200031287fd85fa6074848f
adsfadsfhs <adsfadsfhs@gmail.com>
2009-11-10 16:53:47 -0600

    Introducing LineConverterTest

---
+++ FastBoard/test/fastboard/lineconverter/LineConverterTest.java
@@ -0,0 +1,11 @@
+package fastboard.lineconverter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 10-Nov-2009
+ * Time: 4:53:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LineConverterTest {
+}
