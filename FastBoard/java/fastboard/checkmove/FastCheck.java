b91edc161c277fd3e3d76e308064d5955e64b0b9
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 00:28:39 -0600

    Introducing FastCheck

---
+++ FastBoard/java/fastboard/checkmove/FastCheck.java
@@ -0,0 +1,11 @@
+package fastboard.checkmove;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Nov 22, 2009
+ * Time: 12:28:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface FastCheck {
+}
