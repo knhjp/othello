48e465577c56c2bfcd4b5176ab29a0985d52a673
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 00:59:16 -0600

    Created FastCheck11BlackTest

---
+++ FastBoard/test/fastboard/checkmove/fastcheck/FastCheck11BlackTest.java
@@ -0,0 +1,11 @@
+package fastboard.checkmove.fastcheck;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Nov 22, 2009
+ * Time: 12:59:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastCheck11BlackTest {
+}
