c95f4f62c8a3b5118156480c675f11c8e6147b98
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 00:22:08 -0600

    updated description

--- FastBoard/java/fastboard/checkmove/linedecoder/LineDecoder.java
+++ FastBoard/java/fastboard/checkmove/linedecoder/LineDecoder.java
@@ -5,7 +5,7 @@ package fastboard.checkmove.linedecoder;
  * User: knhjp
  * Date: Nov 7, 2009
  * Time: 7:58:23 PM
- * This helper class it used to determine whether or not a disc is of a particular color
+ * This helper class it used to determine whether or not a disc is of a particular color for a given line
  */
 public interface LineDecoder {
     LineDecoder[] decoders = new LineDecoder[]{
