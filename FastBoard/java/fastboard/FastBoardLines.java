b13b1562a51d2cb1ebcd4e5b9693bf53a110d150
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 00:52:24 -0600

    change field variables to public so I can have direct access

--- FastBoard/java/fastboard/FastBoardLines.java
+++ FastBoard/java/fastboard/FastBoardLines.java
@@ -12,48 +12,48 @@ public class FastBoardLines {
 
     //for example: 0 = the whole line is empty. 1 = _______x, 2 = _______o, 3 = ______x_
     //horizonal
-    protected int a1_h1;
-    protected int a2_h2;
-    protected int a3_h3;
-    protected int a4_h4;
-    protected int a5_h5;
-    protected int a6_h6;
-    protected int a7_h7;
-    protected int a8_h8;
+    public int a1_h1;
+    public int a2_h2;
+    public int a3_h3;
+    public int a4_h4;
+    public int a5_h5;
+    public int a6_h6;
+    public int a7_h7;
+    public int a8_h8;
 
     //vertical
-    protected int a1_a8;
-    protected int b1_b8;
-    protected int c1_c8;
-    protected int d1_d8;
-    protected int e1_e8;
-    protected int f1_f8;
-    protected int g1_g8;
-    protected int h1_h8;
+    public int a1_a8;
+    public int b1_b8;
+    public int c1_c8;
+    public int d1_d8;
+    public int e1_e8;
+    public int f1_f8;
+    public int g1_g8;
+    public int h1_h8;
 
     // diagonal /
-    protected int a3_c1;
-    protected int a4_d1;
-    protected int a5_e1;
-    protected int a6_f1;
-    protected int a7_g1;
-    protected int a8_h1;
-    protected int b8_h2;
-    protected int c8_h3;
-    protected int d8_h4;
-    protected int e8_h5;
-    protected int f8_h6;
+    public int a3_c1;
+    public int a4_d1;
+    public int a5_e1;
+    public int a6_f1;
+    public int a7_g1;
+    public int a8_h1;
+    public int b8_h2;
+    public int c8_h3;
+    public int d8_h4;
+    public int e8_h5;
+    public int f8_h6;
 
     //diagonal \
-    protected int f1_h3;
-    protected int e1_h4;
-    protected int d1_h5;
-    protected int c1_h6;
-    protected int b1_h7;
-    protected int a1_h8;
-    protected int a2_g8;
-    protected int a3_f8;
-    protected int a4_e8;
-    protected int a5_d8;
-    protected int a6_c8;
+    public int f1_h3;
+    public int e1_h4;
+    public int d1_h5;
+    public int c1_h6;
+    public int b1_h7;
+    public int a1_h8;
+    public int a2_g8;
+    public int a3_f8;
+    public int a4_e8;
+    public int a5_d8;
+    public int a6_c8;
 }
