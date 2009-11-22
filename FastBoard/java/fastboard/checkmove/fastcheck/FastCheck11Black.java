f53e6bf5a93b8cb071e7150b57e075d45db90cd4
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 00:51:21 -0600

    create field

--- FastBoard/java/fastboard/checkmove/fastcheck/FastCheck11Black.java
+++ FastBoard/java/fastboard/checkmove/fastcheck/FastCheck11Black.java
@@ -11,7 +11,10 @@ import fastboard.FastBoardLines;
  * This class tells you, given the current configuration, whether or not 11 (a1) is a valid move
  */
 public class FastCheck11Black implements FastCheck {
+    private boolean[][] fastCheckCalcArray;
+
     public FastCheck11Black(boolean[][] fastCheckCalcArray) {
+        this.fastCheckCalcArray = fastCheckCalcArray;
     }
 
     @Override public boolean isValidMove(FastBoardLines lines) {
