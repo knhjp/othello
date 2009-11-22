f6aff867c65264fa3e6ff1c56881127079d64495
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 01:03:30 -0600

    Introducing FastBoardFlipsTest

---
+++ FastBoard/test/fastboard/FastBoardFlipsTest.java
@@ -0,0 +1,11 @@
+package fastboard;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Nov 22, 2009
+ * Time: 1:03:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastBoardFlipsTest {
+}
