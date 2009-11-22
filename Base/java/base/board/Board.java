148e88b215b7a3654f17a9e187c7f93abb99d913
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 00:29:59 -0600

    updated java doc

--- Base/java/base/board/Board.java
+++ Base/java/base/board/Board.java
@@ -22,7 +22,7 @@ public interface Board {
 
     /**
      * Determines whether or not a valid move can be made in certain location of the board
-     * @param color the color that is the next to move
+     * @param color the color that is the next to move -- -1 for white and 1 for black
      * @param location The location of the board you would like to know if it is a valid move  @return true if a move can be made here, false otherwise
      * @return true if move is valid, force otherwise
      */
