105170e02c7e810a79c8261182e4646c40ae29a6
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 00:50:47 -0600

    added constructor

--- FastBoard/java/fastboard/checkmove/fastcheck/FastCheck11Black.java
+++ FastBoard/java/fastboard/checkmove/fastcheck/FastCheck11Black.java
@@ -11,6 +11,9 @@ import fastboard.FastBoardLines;
  * This class tells you, given the current configuration, whether or not 11 (a1) is a valid move
  */
 public class FastCheck11Black implements FastCheck {
+    public FastCheck11Black() {
+    }
+
     @Override public boolean isValidMove(FastBoardLines lines) {
         return false;  //To change body of implemented methods use File | Settings | File Templates.
     }
