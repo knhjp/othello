03717cdf0d4c759215d207497633ea34993b254c
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-11-22 00:48:42 -0600

    Added description

--- FastBoard/java/fastboard/checkmove/fastcheck/FastCheck11Black.java
+++ FastBoard/java/fastboard/checkmove/fastcheck/FastCheck11Black.java
@@ -5,7 +5,7 @@ package fastboard.checkmove.fastcheck;
  * User: knhjp
  * Date: Nov 22, 2009
  * Time: 12:47:58 AM
- * To change this template use File | Settings | File Templates.
+ * This class tells you, given the current configuration, whether or not 11 (a1) is a valid move
  */
 public class FastCheck11Black {
 }
