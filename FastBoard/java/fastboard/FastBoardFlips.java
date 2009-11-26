caa25c17970149a7dfa6e5f01f175a0c604fb60c
adsfadsfhs <adsfadsfhs@gmail.com>
2009-11-26 15:16:56 -0600

    no flipping for h8 as well

--- FastBoard/java/fastboard/FastBoardFlips.java
+++ FastBoard/java/fastboard/FastBoardFlips.java
@@ -15,23 +15,11 @@ public class FastBoardFlips extends FastBoardLines {
         a1_a8+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
     }
 
-//    public void blackFlipA1() {
-//        a1_h1-=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
-//        a1_h8-=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
-//        a1_a8-=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
-//    }
-
     public void whitePlaceA1() {
         a1_h1+=2 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
         a1_h8+=2 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
         a1_a8+=2 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
     }
-
-//    public void whiteFlipA1() {
-//        a1_h1+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
-//        a1_h8+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
-//        a1_a8+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
-//    }
 
     //a2
     public void blackPlaceA2() {
@@ -65,21 +53,9 @@ public class FastBoardFlips extends FastBoardLines {
         h1_h8+=1;
     }
 
-    public void blackFlipH8() {
-        a8_h8-=1;
-        a1_h8-=1;
-        h1_h8-=1;
-    }
-
     public void whitePlaceH8() {
         a8_h8+=2;
         a1_h8+=2;
         h1_h8+=2;
-    }
-
-    public void whiteFlipH8() {
-        a8_h8+=1;
-        a1_h8+=1;
-        h1_h8+=1;
     }
 }
