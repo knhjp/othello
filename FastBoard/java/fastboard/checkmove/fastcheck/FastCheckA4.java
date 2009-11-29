5196beeb310d2696b52933e97906a436ed83f4f0
adsfadsfhs <adsfadsfhs@gmail.com>
2009-11-29 15:20:41 -0600

    code for checking whether or not a4 is a valid move

---
+++ FastBoard/java/fastboard/checkmove/fastcheck/FastCheckA4.java
@@ -0,0 +1,27 @@
+package fastboard.checkmove.fastcheck;
+
+import fastboard.checkmove.FastCheck;
+import fastboard.FastBoardLines;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Nov 29, 2009
+ * Time: 03:19:22 PM
+ * This class tells you, given the current configuration, whether or not a4 is a valid move
+ */
+public class FastCheckA4 implements FastCheck {
+    private boolean[][] fastCheckCalcArray;
+
+    public FastCheckA4(boolean[][] fastCheckCalcArray) {
+        this.fastCheckCalcArray = fastCheckCalcArray;
+    }
+
+    @Override public boolean isValidMove(FastBoardLines lines) {
+        return
+                fastCheckCalcArray[4][lines.a1_a8] ||
+                fastCheckCalcArray[7][lines.a4_h4] ||
+                fastCheckCalcArray[4][lines.a4_e8] ||
+                fastCheckCalcArray[3][lines.a4_d1];
+    }
+}
