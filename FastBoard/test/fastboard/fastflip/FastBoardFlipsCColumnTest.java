d294b049ff1028ca84c35dad9911f8ceb5380868
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-12-02 20:48:18 -0600

    introducing FastBoardFlipsCColumnTest for testing the flipping of the C column

---
+++ FastBoard/test/fastboard/fastflip/FastBoardFlipsCColumnTest.java
@@ -0,0 +1,15 @@
+package fastboard.fastflip;
+
+import base.testcase.OthelloTestCase;
+import fastboard.lineconverter.LineConverter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Dec 2, 2009
+ * Time: 8:47:33 AM
+ * This class tests whether or not FastBoardFlip does what it is supposed to do for column C
+ */
+public class FastBoardFlipsCColumnTest extends OthelloTestCase {
+    
+}
