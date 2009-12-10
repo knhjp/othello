8e912ac9371ff8836de374ace80cd4a5a3620a38
edmund <knhjp@adsfadsfhs-desktop.(none)>
2009-12-09 20:37:51 -0600

    Introducing FastBoardFlipsDColumnTest

---
+++ FastBoard/java/fastboard/fastflip/FastBoardFlipsDColumnTest.java
@@ -0,0 +1,11 @@
+package fastboard.fastflip;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Dec 9, 2009
+ * Time: 8:37:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastBoardFlipsDColumnTest {
+}
