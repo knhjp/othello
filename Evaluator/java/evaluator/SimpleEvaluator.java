c31df81c53f5bb65f6bcee494bb35b4e9a8d3310
adsfadsfhs <adsfadsfhs@gmail.com>
2009-12-22 17:11:46 -0600

    Introducing SimpleEvaluator

---
+++ Evaluator/java/evaluator/SimpleEvaluator.java
@@ -0,0 +1,11 @@
+package evaluator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: 22-Dec-2009
+ * Time: 5:11:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleEvaluator {
+}
