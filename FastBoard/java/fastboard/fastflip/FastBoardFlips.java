0677be2180301db268c332ee65dd9652afb76a89
ed <knhjp@oldcomp-desktop.(none)>
2009-12-23 20:09:07 -0600

    code for flipping and placing f2

--- FastBoard/java/fastboard/fastflip/FastBoardFlips.java
+++ FastBoard/java/fastboard/fastflip/FastBoardFlips.java
@@ -1151,6 +1151,35 @@ public class FastBoardFlips extends FastBoardLines {
         f1_f8+=1 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
     }
 
+    //f2
+    public void blackPlaceF2() {
+        a2_h2+=1 * 3 * 3;
+        e1_h4+=1 * 3 * 3;
+        a7_g1+=1 * 3;
+        f1_f8+=1 * 3 * 3 * 3 * 3 * 3 * 3;
+    }
+
+    public void blackFlipF2() {
+        a2_h2-=1 * 3 * 3;
+        e1_h4-=1 * 3 * 3;
+        a7_g1-=1 * 3;
+        f1_f8-=1 * 3 * 3 * 3 * 3 * 3 * 3;
+    }
+
+    public void whitePlaceF2() {
+        a2_h2+=2 * 3 * 3;
+        e1_h4+=2 * 3 * 3;
+        a7_g1+=2 * 3;
+        f1_f8+=2 * 3 * 3 * 3 * 3 * 3 * 3;
+    }
+
+    public void whiteFlipF2() {
+        a2_h2+=1 * 3 * 3;
+        e1_h4+=1 * 3 * 3;
+        a7_g1+=1 * 3;
+        f1_f8+=1 * 3 * 3 * 3 * 3 * 3 * 3;
+    }
+
     //h8
     public void blackPlaceH8() {
         a8_h8+=1;
