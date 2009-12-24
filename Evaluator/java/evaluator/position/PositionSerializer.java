f39910589a712140ff8f108ec78e5150b8768dfd
ed <knhjp@oldcomp-desktop.(none)>
2009-12-23 19:12:26 -0600

    introducing PositionSerializer

---
+++ Evaluator/java/evaluator/position/PositionSerializer.java
@@ -0,0 +1,11 @@
+package evaluator.position;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Dec 23, 2009
+ * Time: 7:09:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PositionSerializer {
+}
