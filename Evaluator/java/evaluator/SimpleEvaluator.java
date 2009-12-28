8c9696cbf027d686a0d8cbb125988c18180da71c
adsfadsfhs <adsfadsfhs@gmail.com>
2009-12-28 17:32:18 -0600

    simple eval now implements evaluator

--- Evaluator/java/evaluator/SimpleEvaluator.java
+++ Evaluator/java/evaluator/SimpleEvaluator.java
@@ -1,5 +1,8 @@
 package evaluator;
 
+import base.board.Board;
+import base.eval.Evaluator;
+
 /**
  * Created by IntelliJ IDEA.
  * User: knhjp
@@ -7,5 +10,8 @@ package evaluator;
  * Time: 5:11:24 PM
  * This class is meant to be a simple version to see how we should implement an evaluator
  */
-public class SimpleEvaluator {
+public class SimpleEvaluator implements Evaluator {
+    @Override public double eval(Board board) {
+        return 0;
+    }
 }
