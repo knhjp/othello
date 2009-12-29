f3e7519480840da93698728f2d138ad5a24e88d0
ed <knhjp@oldcomp-desktop.(none)>
2009-12-28 20:26:10 -0600

    Introducing PositionOnDisk

---
+++ Evaluator/java/evaluator/position/PositionOnDisk.java
@@ -0,0 +1,11 @@
+package evaluator.position;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Dec 28, 2009
+ * Time: 8:25:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PositionOnDisk {
+}
