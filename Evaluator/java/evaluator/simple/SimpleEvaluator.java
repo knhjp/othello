8b19e542288eb5cb6ed1d3082af4f38d0bf5f1c5
adsfadsfhs <adsfadsfhs@gmail.com>
2009-12-30 13:47:24 -0600

    make SimpleEvaluator externalizable

--- Evaluator/java/evaluator/simple/SimpleEvaluator.java
+++ Evaluator/java/evaluator/simple/SimpleEvaluator.java
@@ -3,6 +3,11 @@ package evaluator.simple;
 import base.board.Board;
 import base.eval.Evaluator;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
 /**
  * Created by IntelliJ IDEA.
  * User: knhjp
@@ -10,12 +15,21 @@ import base.eval.Evaluator;
  * Time: 5:11:24 PM
  * This class is meant to be a simple version to see how we should implement an evaluator
  */
-public class SimpleEvaluator implements Evaluator {
+public class SimpleEvaluator implements Evaluator, Externalizable {
     //for externalization
     public SimpleEvaluator() {
     }
 
     @Override public double eval(Board board) {
         return 0;
+    }
+
+    @Override public void writeExternal(ObjectOutput objectOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput objectInput) throws IOException, ClassNotFoundException {
+        
     }
 }
