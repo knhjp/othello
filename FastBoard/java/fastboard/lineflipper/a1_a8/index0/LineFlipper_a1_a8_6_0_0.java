694f159d31ec9df324130ee791f449130ed80c4e
ed <ed@emachines-laptop.(none)>
2010-01-02 07:51:24 -0800

    my first attempt to flipping some discs

--- FastBoard/java/fastboard/lineflipper/a1_a8/index0/LineFlipper_a1_a8_6_0_0.java
+++ FastBoard/java/fastboard/lineflipper/a1_a8/index0/LineFlipper_a1_a8_6_0_0.java
@@ -1,5 +1,8 @@
 package fastboard.lineflipper.a1_a8.index0;
 
+import fastboard.lineflipper.LineFlipper;
+import fastboard.fastflip.FastBoardFlips;
+
 /**
  * Created by IntelliJ IDEA.
  * User: ed
@@ -7,5 +10,14 @@ package fastboard.lineflipper.a1_a8.index0;
  * Time: 7:37:03 AM
  * This flips along a1_a8, 6 discs for index 0 (a8)
  */
-public class LineFlipper_a1_a8_6_0_0 {
+public class LineFlipper_a1_a8_6_0_0 implements LineFlipper {
+    @Override public int flipLine(FastBoardFlips lines) {
+        lines.blackFlipA2();
+        lines.blackFlipA3();
+        lines.blackFlipA4();
+        lines.blackFlipA5();
+        lines.blackFlipA6();
+        lines.blackFlipA7();
+        return 12;
+    }
 }
