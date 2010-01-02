e48c32c2826e3cdb88d7855a5960c53844269ea1
ed <ed@emachines-laptop.(none)>
2010-01-02 07:54:40 -0800

    updated description

--- FastBoard/java/fastboard/lineflipper/a1_a8/index1/LineFlipper_a1_a8_5_0_1_Black.java
+++ FastBoard/java/fastboard/lineflipper/a1_a8/index1/LineFlipper_a1_a8_5_0_1_Black.java
@@ -5,7 +5,7 @@ package fastboard.lineflipper.a1_a8.index1;
  * User: ed
  * Date: Jan 2, 2010
  * Time: 7:42:30 AM
- * This flips along a1_a8, 5 discs for index 1 (a7)
+ * This flips along a1_a8, 5 discs for index 1 (a7) for black
  */
 public class LineFlipper_a1_a8_5_0_1_Black {
 }
