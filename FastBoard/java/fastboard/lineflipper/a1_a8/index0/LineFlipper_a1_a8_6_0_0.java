6ce0d559efdcf9e28d2fd7c5adc38fe75c74adb9
ed <ed@emachines-laptop.(none)>
2010-01-02 07:43:42 -0800

    added description

--- FastBoard/java/fastboard/lineflipper/a1_a8/index0/LineFlipper_a1_a8_6_0_0.java
+++ FastBoard/java/fastboard/lineflipper/a1_a8/index0/LineFlipper_a1_a8_6_0_0.java
@@ -5,7 +5,7 @@ package fastboard.lineflipper.a1_a8.index0;
  * User: ed
  * Date: Jan 2, 2010
  * Time: 7:37:03 AM
- * To change this template use File | Settings | File Templates.
+ * This flips along a1_a8, 6 discs for index 0 (a8)
  */
 public class LineFlipper_a1_a8_6_0_0 {
 }
