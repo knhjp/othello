8d336c1705baf7d8d23127d46a09afba2dfe3146
ed <ed@emachines-laptop.(none)>
2010-01-02 07:37:39 -0800

    Introducing LineFlipper_a1_a8_0_6_0

---
+++ FastBoard/java/fastboard/lineflipper/a1_a8/LineFlipper_a1_a8_0_6_0.java
@@ -0,0 +1,11 @@
+package fastboard.lineflipper.a1_a8;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ed
+ * Date: Jan 2, 2010
+ * Time: 7:37:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LineFlipper_a1_a8_0_6_0 {
+}
