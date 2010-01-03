b40fa3a55be0090c720a3bdd9b8a06da393e073f
adsfadsfhs <adsfadsfhs@gmail.com>
2010-01-03 14:48:52 -0600

    test for FastCheckD1 for white

--- FastBoard/test/fastboard/checkmove/fastcheck/dcolumn/FastCheckD1Test.java
+++ FastBoard/test/fastboard/checkmove/fastcheck/dcolumn/FastCheckD1Test.java
@@ -47,4 +47,39 @@ public class FastCheckD1Test extends OthelloTestCase {
         flips.a4_d1 = LineConverter.convertStringToLine("_____xo_");
         assertTrue(check.isValidMove(flips));
     }
+
+    public void testIsMoveValidWhite() {
+        FastCheckCalc calc = new FastCheckCalc();
+        boolean[][] fastCheckCalcArray = calc.calcIsMoveValidForWhite();
+        FastCheckD1 check = new FastCheckD1(fastCheckCalcArray);
+
+        FastBoardFlips flips = new FastBoardFlips();
+
+        assertFalse(check.isValidMove(flips));
+
+        flips.d1_d8 = LineConverter.convertStringToLine("_xxxxxxx");
+        assertFalse(check.isValidMove(flips));
+        flips.d1_d8 = LineConverter.convertStringToLine("_xxxxxxo");
+        assertTrue(check.isValidMove(flips));
+
+        flips.d1_d8 = LineConverter.convertStringToLine("_xxxx_ox");
+        flips.d1_h5 = LineConverter.convertStringToLine("___xx_x_");
+        assertFalse(check.isValidMove(flips));
+        flips.d1_h5 = LineConverter.convertStringToLine("____xox_");
+        assertTrue(check.isValidMove(flips));
+
+        flips.d1_h5 = LineConverter.convertStringToLine("____x_oo");
+        flips.a1_h1 = LineConverter.convertStringToLine("____oxxo");
+        assertFalse(check.isValidMove(flips));
+        flips.a1_h1 = LineConverter.convertStringToLine("____xo__");
+        assertTrue(check.isValidMove(flips));
+        flips.a1_h1 = LineConverter.convertStringToLine("_ox_____");
+        assertTrue(check.isValidMove(flips));
+
+        flips.a1_h1 = LineConverter.convertStringToLine("____ox__");
+        flips.a4_d1 = LineConverter.convertStringToLine("_____xo_");
+        assertFalse(check.isValidMove(flips));
+        flips.a4_d1 = LineConverter.convertStringToLine("_____ox_");
+        assertTrue(check.isValidMove(flips));
+    }
 }
