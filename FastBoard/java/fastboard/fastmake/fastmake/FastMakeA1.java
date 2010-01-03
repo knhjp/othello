61c86698164fb277ccb8900ad454d4036658e422
ed <ed@emachines-laptop.(none)>
2010-01-03 14:24:45 -0800

    Introducing FastMakeA1

---
+++ FastBoard/java/fastboard/fastmake/fastmake/FastMakeA1.java
@@ -0,0 +1,11 @@
+package fastboard.fastmake.fastmake;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ed
+ * Date: Jan 3, 2010
+ * Time: 2:24:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastMakeA1 {
+}
