06bfee6781bed5fba5ac649cabc2eb52f1353e8e
adsfadsfhs <adsfadsfhs@gmail.com>
2010-01-03 13:39:51 -0600

    code for checking whether or not we determine b8 is a valid move correctly for black

---
+++ FastBoard/test/fastboard/checkmove/fastcheck/bcolumn/FastCheckB8Test.java
@@ -0,0 +1,42 @@
+package fastboard.checkmove.fastcheck.bcolumn;
+
+import base.testcase.OthelloTestCase;
+import fastboard.checkmove.calc.FastCheckCalc;
+import fastboard.fastflip.FastBoardFlips;
+import fastboard.lineconverter.LineConverter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Nov 29, 2009
+ * Time: 02:53:25 PM
+ * Tests whether or not FastCheckA8 checks for valid moves properly
+ */
+public class FastCheckB8Test extends OthelloTestCase {
+    public void testIsMoveValidBlack() {
+        FastCheckCalc calc = new FastCheckCalc();
+        boolean[][] fastCheckCalcArray = calc.calcIsMoveValidForBlack();
+        FastCheckB8 check = new FastCheckB8(fastCheckCalcArray);
+
+        FastBoardFlips flips = new FastBoardFlips();
+
+        assertFalse(check.isValidMove(flips));
+
+        flips.b1_b8 = LineConverter.convertStringToLine("xoooooox");
+        assertFalse(check.isValidMove(flips));
+        flips.b1_b8 = LineConverter.convertStringToLine("xoooooo_");
+        assertTrue(check.isValidMove(flips));
+
+        flips.b1_b8 = LineConverter.convertStringToLine("_oooo_xo");
+        flips.a8_h8 = LineConverter.convertStringToLine("x_xoooox");
+        assertFalse(check.isValidMove(flips));
+        flips.a8_h8 = LineConverter.convertStringToLine("__oox___");
+        assertTrue(check.isValidMove(flips));
+
+        flips.a8_h8 = LineConverter.convertStringToLine("_xooxoxo");
+        flips.b8_h2 = LineConverter.convertStringToLine("__xoooox");
+        assertFalse(check.isValidMove(flips));
+        flips.b8_h2 = LineConverter.convertStringToLine("__ooooox");
+        assertTrue(check.isValidMove(flips));
+    }
+}
