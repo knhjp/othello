194fa268a300312a823e64eb3bc01140f2ebf4e2
ed <ed@emachines-laptop.(none)>
2010-01-03 14:26:15 -0800

    implements interface

--- FastBoard/java/fastboard/fastmake/fastmake/FastMakeA1.java
+++ FastBoard/java/fastboard/fastmake/fastmake/FastMakeA1.java
@@ -1,5 +1,8 @@
 package fastboard.fastmake.fastmake;
 
+import fastboard.fastmake.FastMake;
+import fastboard.FastBoardLines;
+
 /**
  * Created by IntelliJ IDEA.
  * User: ed
@@ -7,5 +10,8 @@ package fastboard.fastmake.fastmake;
  * Time: 2:24:19 PM
  * This updates the current board depending on the existing position for A1
  */
-public class FastMakeA1 {
+public class FastMakeA1 implements FastMake {
+    @Override public int makeMove(FastBoardLines lines) {
+        return 0;
+    }
 }
