71f05172cc20a3401f3146408b8575be81a52f4d
ed <ed@emachines-laptop.(none)>
2010-01-03 14:19:21 -0800

    added description

--- FastBoard/java/fastboard/fastmake/FastMake.java
+++ FastBoard/java/fastboard/fastmake/FastMake.java
@@ -5,7 +5,7 @@ package fastboard.fastmake;
  * User: ed
  * Date: Jan 3, 2010
  * Time: 2:18:32 PM
- * To change this template use File | Settings | File Templates.
+ * This interface is meant to be like FastFlip, providing a way to make moves based on the line configurations
  */
 public interface FastMake {
 }
