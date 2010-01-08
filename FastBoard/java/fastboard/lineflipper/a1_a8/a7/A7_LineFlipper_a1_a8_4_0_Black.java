1f13f95c330b336654c2005d4de5d26ff6708f14
ed <knhjp@oldcomp-desktop.(none)>
2010-01-07 19:10:55 -0600

    created a version for flipping only 4 discs

---
+++ FastBoard/java/fastboard/lineflipper/a1_a8/a7/A7_LineFlipper_a1_a8_4_0_Black.java
@@ -0,0 +1,21 @@
+package fastboard.lineflipper.a1_a8.a7;
+
+import fastboard.lineflipper.LineFlipper;
+import fastboard.fastflip.FastBoardFlips;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ed
+ * Date: Jan 2, 2010
+ * Time: 7:42:30 AM
+ * This flips along a1_a8, 5 discs for a7 for black
+ */
+public class A7_LineFlipper_a1_a8_4_0_Black implements LineFlipper {
+    @Override public int flipLine(FastBoardFlips lines) {
+        lines.blackFlipA3();
+        lines.blackFlipA4();
+        lines.blackFlipA5();
+        lines.blackFlipA6();
+        return 8;
+    }
+}
