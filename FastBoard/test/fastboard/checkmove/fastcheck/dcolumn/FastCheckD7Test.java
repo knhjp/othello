121f341f4f04efd31865cd340ca86866fd30cb94
adsfadsfhs <adsfadsfhs@gmail.com>
2010-01-09 15:16:08 -0600

    test for whether or not we check d7 correctly for white

--- FastBoard/test/fastboard/checkmove/fastcheck/dcolumn/FastCheckD7Test.java
+++ FastBoard/test/fastboard/checkmove/fastcheck/dcolumn/FastCheckD7Test.java
@@ -45,4 +45,37 @@ public class FastCheckD7Test extends OthelloTestCase {
         flips.a4_e8 = LineConverter.convertStringToLine("____xo__");
         assertTrue(check.isValidMove(flips));
     }
+
+    public void testIsMoveValidWhite() {
+        FastCheckCalc calc = new FastCheckCalc();
+        boolean[][] fastCheckCalcArray = calc.calcIsMoveValidForWhite();
+        FastCheckD7 check = new FastCheckD7(fastCheckCalcArray);
+
+        FastBoardFlips flips = new FastBoardFlips();
+
+        assertFalse(check.isValidMove(flips));
+
+        flips.d1_d8 = LineConverter.convertStringToLine("oxxxxxxo");
+        assertFalse(check.isValidMove(flips));
+        flips.d1_d8 = LineConverter.convertStringToLine("oxxxxx_o");
+        assertTrue(check.isValidMove(flips));
+
+        flips.d1_d8 = LineConverter.convertStringToLine("_xxxx_ox");
+        flips.a7_h7 = LineConverter.convertStringToLine("____oxxo");
+        assertFalse(check.isValidMove(flips));
+        flips.a7_h7 = LineConverter.convertStringToLine("____xxo_");
+        assertTrue(check.isValidMove(flips));
+
+        flips.a7_h7 = LineConverter.convertStringToLine("____oxox");
+        flips.c8_h3 = LineConverter.convertStringToLine("___xxxxo");
+        assertFalse(check.isValidMove(flips));
+        flips.c8_h3 = LineConverter.convertStringToLine("____xxxo");
+        assertTrue(check.isValidMove(flips));
+
+        flips.c8_h3 = LineConverter.convertStringToLine("_____xxo");
+        flips.a4_e8 = LineConverter.convertStringToLine("____oo__");
+        assertFalse(check.isValidMove(flips));
+        flips.a4_e8 = LineConverter.convertStringToLine("____ox__");
+        assertTrue(check.isValidMove(flips));
+    }
 }
