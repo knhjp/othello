6c288180d503014cd644012c1965a4e2a421f3bd
ed <knhjp@oldcomp-desktop.(none)>
2010-01-08 22:14:24 -0600

    code for flipping two discs along a1_a8 for a6

---
+++ FastBoard/java/fastboard/lineflipper/lines/a1_a8/a6/A6_LineFlipper_a1_a8_2_0_Black.java
@@ -0,0 +1,19 @@
+package fastboard.lineflipper.lines.a1_a8.a6;
+
+import fastboard.lineflipper.LineFlipper;
+import fastboard.fastflip.FastBoardFlips;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ed
+ * Date: Jan 2, 2010
+ * Time: 7:42:30 AM
+ * This flips along a1_a8, 5 discs for a7 for black
+ */
+public class A6_LineFlipper_a1_a8_2_0_Black implements LineFlipper {
+    @Override public int flipLine(FastBoardFlips lines) {
+        lines.blackFlipA4();
+        lines.blackFlipA5();
+        return 4;
+    }
+}
