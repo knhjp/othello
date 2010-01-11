56bd762164fa1d7002b540c3aacbd4be7d8fa81f
ed <knhjp@oldcomp-desktop.(none)>
2010-01-10 20:50:45 -0600

    check for flipping two discs

--- FastBoard/test/fastboard/lineflipper/calc/LineFlipperNumFlipCalcTest.java
+++ FastBoard/test/fastboard/lineflipper/calc/LineFlipperNumFlipCalcTest.java
@@ -29,5 +29,11 @@ public class LineFlipperNumFlipCalcTest extends OthelloTestCase {
         numFlip = calc.numFlipForBlackForThisLine(line, index);
         assertEquals(1,numFlip.upNum);
         assertEquals(0,numFlip.downNum);
+
+        line = LineConverter.convertStringToLine("___xoo__");
+        index = 1;
+        numFlip = calc.numFlipForBlackForThisLine(line, index);
+        assertEquals(2,numFlip.upNum);
+        assertEquals(0,numFlip.downNum);
     }
 }
