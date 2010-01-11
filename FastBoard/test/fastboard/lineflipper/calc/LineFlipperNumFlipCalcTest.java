09cd6b56672519ff9f43f07ebfdc3182b78dd26e
ed <knhjp@oldcomp-desktop.(none)>
2010-01-10 19:49:54 -0600

    added test method

--- FastBoard/test/fastboard/lineflipper/calc/LineFlipperNumFlipCalcTest.java
+++ FastBoard/test/fastboard/lineflipper/calc/LineFlipperNumFlipCalcTest.java
@@ -1,5 +1,7 @@
 package fastboard.lineflipper.calc;
 
+import base.testcase.OthelloTestCase;
+
 /**
  * Created by IntelliJ IDEA.
  * User: ed
@@ -7,5 +9,8 @@ package fastboard.lineflipper.calc;
  * Time: 1:59:41 PM
  * To change this template use File | Settings | File Templates.
  */
-public class LineFlipperNumFlipCalcTest {
+public class LineFlipperNumFlipCalcTest extends OthelloTestCase {
+    public void testNumFlipForBlackForThisLine() {
+
+    }
 }
