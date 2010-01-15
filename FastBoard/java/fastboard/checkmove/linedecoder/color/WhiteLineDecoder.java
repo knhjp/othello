ddf2774d6f73d1eccfce5912096477bda51dcbae
ed <knhjp@oldcomp-desktop.(none)>
2010-01-15 07:51:40 -0600

    WhiteLineDecoder now implements the ColorLineDecoder interface

--- FastBoard/java/fastboard/checkmove/linedecoder/color/WhiteLineDecoder.java
+++ FastBoard/java/fastboard/checkmove/linedecoder/color/WhiteLineDecoder.java
@@ -7,5 +7,18 @@ package fastboard.checkmove.linedecoder.color;
  * Time: 7:02:53 PM
  * To change this template use File | Settings | File Templates.
  */
-public class WhiteLineDecoder {
+public class WhiteLineDecoder implements ColorLineDecoder {
+    @Override public boolean isMyColor(int line) {
+        return false;
+    }
+
+    @Override
+    public boolean isOppColor(int line) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty(int line) {
+        return false;
+    }
 }
