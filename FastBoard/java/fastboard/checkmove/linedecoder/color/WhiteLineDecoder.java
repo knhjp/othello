15d739b58fd32e5e33fc993e3bc92e7298c2c535
ed <ed@emachines-laptop.(none)>
2010-01-15 05:52:26 -0800

    added description for WhiteLineDecoder

--- FastBoard/java/fastboard/checkmove/linedecoder/color/WhiteLineDecoder.java
+++ FastBoard/java/fastboard/checkmove/linedecoder/color/WhiteLineDecoder.java
@@ -5,7 +5,7 @@ package fastboard.checkmove.linedecoder.color;
  * User: ed
  * Date: Jan 14, 2010
  * Time: 7:02:53 PM
- * To change this template use File | Settings | File Templates.
+ * This class decodes a line from the perspective of white
  */
 public class WhiteLineDecoder {
 }
