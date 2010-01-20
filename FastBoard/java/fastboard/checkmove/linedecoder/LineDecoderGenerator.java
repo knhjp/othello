35fde9b421a51e50b4123b38acbf0a2b260833d7
ed <ed@emachines-laptop.(none)>
2010-01-19 18:16:53 -0800

    created decoders

--- FastBoard/java/fastboard/checkmove/linedecoder/LineDecoderGenerator.java
+++ FastBoard/java/fastboard/checkmove/linedecoder/LineDecoderGenerator.java
@@ -8,6 +8,16 @@ package fastboard.checkmove.linedecoder;
  * To change this template use File | Settings | File Templates.
  */
 public class LineDecoderGenerator {
+    private final LineDecoder[] decoders = new LineDecoder[]{
+            new LineDecoder0(),
+            new LineDecoder1(),
+            new LineDecoder2(),
+            new LineDecoder3(),
+            new LineDecoder4(),
+            new LineDecoder5(),
+            new LineDecoder6(),
+            new LineDecoder7(),
+    };
     public LineDecoderGenerator() {
     }
 }
