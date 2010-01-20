308c3752f2b85df21687b568a8c4d238c9338174
ed <ed@emachines-laptop.(none)>
2010-01-19 18:21:23 -0800

    created blackDecoders

--- FastBoard/java/fastboard/checkmove/linedecoder/LineDecoderGenerator.java
+++ FastBoard/java/fastboard/checkmove/linedecoder/LineDecoderGenerator.java
@@ -1,5 +1,7 @@
 package fastboard.checkmove.linedecoder;
 
+import fastboard.checkmove.linedecoder.color.BlackLineDecoder;
+
 /**
  * Created by IntelliJ IDEA.
  * User: knhjp
@@ -8,16 +10,29 @@ package fastboard.checkmove.linedecoder;
  * To change this template use File | Settings | File Templates.
  */
 public class LineDecoderGenerator {
-    private final LineDecoder[] decoders = new LineDecoder[]{
-            new LineDecoder0(),
-            new LineDecoder1(),
-            new LineDecoder2(),
-            new LineDecoder3(),
-            new LineDecoder4(),
-            new LineDecoder5(),
-            new LineDecoder6(),
-            new LineDecoder7(),
-    };
+    private final BlackLineDecoder[] blackDecoders;
+
     public LineDecoderGenerator() {
+        final LineDecoder[] decoders = new LineDecoder[]{
+                new LineDecoder0(),
+                new LineDecoder1(),
+                new LineDecoder2(),
+                new LineDecoder3(),
+                new LineDecoder4(),
+                new LineDecoder5(),
+                new LineDecoder6(),
+                new LineDecoder7(),
+        };
+
+        blackDecoders = new BlackLineDecoder[]{
+                new BlackLineDecoder(decoders[0]),
+                new BlackLineDecoder(decoders[1]),
+                new BlackLineDecoder(decoders[2]),
+                new BlackLineDecoder(decoders[3]),
+                new BlackLineDecoder(decoders[4]),
+                new BlackLineDecoder(decoders[5]),
+                new BlackLineDecoder(decoders[6]),
+                new BlackLineDecoder(decoders[7]),
+        };
     }
 }
