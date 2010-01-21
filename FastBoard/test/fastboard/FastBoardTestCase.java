79fd6e245013fc1b7868e9df7b8ba7f686e1b79a
adsfadsfhs <adsfadsfhs@gmail.com>
2010-01-20 18:16:10 -0600

    added field variables for easy access

--- FastBoard/test/fastboard/FastBoardTestCase.java
+++ FastBoard/test/fastboard/FastBoardTestCase.java
@@ -1,6 +1,9 @@
 package fastboard;
 
 import base.testcase.OthelloTestCase;
+import fastboard.checkmove.linedecoder.LineDecoderGenerator;
+import fastboard.checkmove.linedecoder.color.BlackLineDecoder;
+import fastboard.checkmove.linedecoder.color.WhiteLineDecoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,4 +13,13 @@ import base.testcase.OthelloTestCase;
  * This class contains objects that are commonly used in FastBoard testing
  */
 public class FastBoardTestCase extends OthelloTestCase {
+    protected final BlackLineDecoder[] blackLineDecoders;
+    protected final WhiteLineDecoder[] whiteLineDecoders;
+
+    public FastBoardTestCase() {
+        LineDecoderGenerator generator = new LineDecoderGenerator();
+        this.blackLineDecoders = generator.getBlackLineDecoders();
+        this.whiteLineDecoders = generator.getWhiteLineDecoders();
+
+    }
 }
