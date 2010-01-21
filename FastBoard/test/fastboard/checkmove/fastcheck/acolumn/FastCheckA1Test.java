355d215bc1ab90a7f26bec0bd0204730ea1b0579
adsfadsfhs <adsfadsfhs@gmail.com>
2010-01-21 17:26:36 -0600

    convert FastCheckA2Test to use color line decoders

--- FastBoard/test/fastboard/checkmove/fastcheck/acolumn/FastCheckA1Test.java
+++ FastBoard/test/fastboard/checkmove/fastcheck/acolumn/FastCheckA1Test.java
@@ -1,6 +1,5 @@
 package fastboard.checkmove.fastcheck.acolumn;
 
-import base.testcase.OthelloTestCase;
 import fastboard.checkmove.calc.FastCheckCalc;
 import fastboard.fastflip.FastBoardFlips;
 import fastboard.lineconverter.LineConverter;
