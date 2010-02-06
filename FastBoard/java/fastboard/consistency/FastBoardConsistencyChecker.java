7a258f17d8f55e6d807dc74ddaf8b0b075efeb0a
ed <ed@emachines-laptop.(none)>
2010-02-06 11:03:20 -0800

    added description

--- FastBoard/java/fastboard/consistency/FastBoardConsistencyChecker.java
+++ FastBoard/java/fastboard/consistency/FastBoardConsistencyChecker.java
@@ -5,7 +5,8 @@ package fastboard.consistency;
  * User: ed
  * Date: Feb 6, 2010
  * Time: 10:58:04 AM
- * To change this template use File | Settings | File Templates.
+ * This class is meant to check the consistency of a FastBoard (a1 is the same status for all lines and
+ * blackMinusWhite should be consistent with the line configurations)
  */
 public class FastBoardConsistencyChecker {
 }
