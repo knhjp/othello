57967a77ac39a78adc301d4c51f4767ad0fa9398
ed <ed@emachines-laptop.(none)>
2010-02-06 12:04:55 -0800

    added field variable checkers

--- FastBoard/java/fastboard/consistency/FastBoardConsistencyChecker.java
+++ FastBoard/java/fastboard/consistency/FastBoardConsistencyChecker.java
@@ -11,7 +11,10 @@ import fastboard.FastBoard;
  * blackMinusWhite should be consistent with the line configurations)
  */
 public class FastBoardConsistencyChecker {
+    private final FastBoardSquareChecker[] checkers;
+
     public FastBoardConsistencyChecker(FastBoardSquareChecker[] checkers) {
+        this.checkers = checkers;
     }
 
     public boolean isConsistent(FastBoard fastBoard) {
