e7ea7ed1f5b116c40abf0cd66d1c7d39137366a4
ed <ed@emachines-laptop.(none)>
2010-02-06 11:14:17 -0800

    Introducing FastBoardSquareChecker

---
+++ FastBoard/java/fastboard/consistency/FastBoardSquareChecker.java
@@ -0,0 +1,11 @@
+package fastboard.consistency;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ed
+ * Date: Feb 6, 2010
+ * Time: 11:14:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface FastBoardSquareChecker {
+}
