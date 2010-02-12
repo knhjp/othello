51c3dc71f50958e00e121a5110e471634dce84c1
ed <ed@emachines-laptop.(none)>
2010-02-12 11:30:41 -0800

    Introducing FastBoardA1Checker

---
+++ FastBoard/java/fastboard/consistency/square/FastBoardA1Checker.java
@@ -0,0 +1,11 @@
+package fastboard.consistency.square;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ed
+ * Date: Feb 12, 2010
+ * Time: 11:30:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FastBoardA1Checker {
+}
