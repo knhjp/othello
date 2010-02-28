9d4914a74f979e38c98199af128e899c0baf98fc
ed <knhjp@oldcomp-desktop.(none)>
2010-02-28 16:42:42 -0600

    passed in more line flippers

--- FastBoard/java/fastboard/lineflipper/squares/A1LineFlipper.java
+++ FastBoard/java/fastboard/lineflipper/squares/A1LineFlipper.java
@@ -15,7 +15,10 @@ public class A1LineFlipper {
     public final LineFlipper[] a1_lineFlippers_a1_h1 = null;
     public final LineFlipper[] a1_lineFlippers_a1_h8 = null;
 
-    public A1LineFlipper(NumFlip[][] numFlips, LineFlipper[][] flippers_a1_a8) {
+    public A1LineFlipper(NumFlip[][] numFlips,
+                         LineFlipper[][] flippers_a1_a8,
+                         LineFlipper[][] flippers_a1_h1,
+                         LineFlipper[][] flippers_a1_h8) {
         a1_lineFlippers_a1_a8 = new LineFlipper[3 * 3 * 3 * 3 * 3 * 3 * 3 * 3];
         for (int i=0 ; i< a1_lineFlippers_a1_a8.length; i++) {
             NumFlip curNumFlip = numFlips[7][i];
