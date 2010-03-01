837e388f8e51e9e1b8f932e9cbee9ec7e5d4cefc
ed <knhjp@oldcomp-desktop.(none)>
2010-02-28 21:01:24 -0600

    Introducing AbstractFastMakeA1

---
+++ FastBoard/java/fastboard/fastmake/fastmake/AbstractFastMakeA1.java
@@ -0,0 +1,11 @@
+package fastboard.fastmake.fastmake;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Feb 28, 2010
+ * Time: 9:01:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AbstractFastMakeA1 {
+}
