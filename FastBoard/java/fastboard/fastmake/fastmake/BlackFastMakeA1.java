c695a07fcc73dd676a36f16320d528c1871febfb
ed <knhjp@oldcomp-desktop.(none)>
2010-02-28 21:06:38 -0600

    moved all the logic to the abstract class

--- FastBoard/java/fastboard/fastmake/fastmake/BlackFastMakeA1.java
+++ FastBoard/java/fastboard/fastmake/fastmake/BlackFastMakeA1.java
@@ -1,9 +1,6 @@
 package fastboard.fastmake.fastmake;
 
-import fastboard.fastflip.FastBoardFlips;
-import fastboard.lineflipper.LineFlipper;
 import fastboard.lineflipper.calc.NumFlip;
-import fastboard.lineflipper.squares.A1LineFlipper;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,23 +10,9 @@ import fastboard.lineflipper.squares.A1LineFlipper;
  * This updates the current board depending on the existing position for A1
  */
 public class BlackFastMakeA1 extends AbstractFastMakeA1 {
-    private final A1LineFlipper lineFlipper;
+
 
     public BlackFastMakeA1(NumFlip[][] numFlips) {
-        LineFlipper[][] blackFlippers_a1_a8 = new LineFlipper[][]{{}};
-        LineFlipper[][] blackFlippers_a1_h1 = new LineFlipper[][]{{}};
-        LineFlipper[][] blackFlippers_a1_h8 = new LineFlipper[][]{{}};
-        this.lineFlipper = new A1LineFlipper(numFlips,blackFlippers_a1_a8,
-                blackFlippers_a1_h1,blackFlippers_a1_h8);
-    }
-
-    @Override public int makeMove(FastBoardFlips lines) {
-        int discDiff =
-                lineFlipper.a1_lineFlippers_a1_a8[lines.a1_a8].flipLine(lines) +
-                lineFlipper.a1_lineFlippers_a1_h1[lines.a1_h1].flipLine(lines) +
-                lineFlipper.a1_lineFlippers_a1_h8[lines.a1_h8].flipLine(lines);
-        lines.blackPlaceA1();
-        discDiff +=1;
-        return discDiff;
+        super(numFlips);
     }
 }
