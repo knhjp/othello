023983c253a3ee8f33ba77b4e86bde3da04518e5
ed <knhjp@oldcomp-desktop.(none)>
2010-03-01 20:12:11 -0600

    filled in the meat for the class

--- FastBoard/java/fastboard/lineflipper/lines/a1_a8/a1/A1_LineFlipper_a1_a8_0_5_Black.java
+++ FastBoard/java/fastboard/lineflipper/lines/a1_a8/a1/A1_LineFlipper_a1_a8_0_5_Black.java
@@ -1,11 +1,22 @@
 package fastboard.lineflipper.lines.a1_a8.a1;
 
+import fastboard.fastflip.FastBoardFlips;
+import fastboard.lineflipper.LineFlipper;
+
 /**
  * Created by IntelliJ IDEA.
  * User: knhjp
  * Date: Mar 1, 2010
  * Time: 8:09:12 PM
- * To change this template use File | Settings | File Templates.
+ * This flips along a1_a8, 5 discs for a1 for black
  */
-public class A1_LineFlipper_a1_a8_0_5_Black {
+public class A1_LineFlipper_a1_a8_0_5_Black implements LineFlipper {
+    @Override public int flipLine(FastBoardFlips lines) {
+        lines.blackFlipA2();
+        lines.blackFlipA3();
+        lines.blackFlipA4();
+        lines.blackFlipA5();
+        lines.blackFlipA6();
+        return 10;
+    }
 }
