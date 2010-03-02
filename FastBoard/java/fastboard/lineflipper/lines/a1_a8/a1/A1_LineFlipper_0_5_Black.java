015ba0acbf5b6f3c748b872b4c2da562aa9ea57a
ed <knhjp@oldcomp-desktop.(none)>
2010-03-01 20:09:29 -0600

    Introducing A1_LineFlipper_0_5_Black

---
+++ FastBoard/java/fastboard/lineflipper/lines/a1_a8/a1/A1_LineFlipper_0_5_Black.java
@@ -0,0 +1,11 @@
+package fastboard.lineflipper.lines.a1_a8.a1;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Mar 1, 2010
+ * Time: 8:09:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class A1_LineFlipper_0_5_Black {
+}
