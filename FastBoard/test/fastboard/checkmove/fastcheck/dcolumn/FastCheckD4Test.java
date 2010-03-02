da10a1a20f5ed0bbc71aa1cc8bada4228b1aa3c9
adsfadsfhs <adsfadsfhs@gmail.com>
2010-03-01 18:06:34 -0600

    converted FastCheckD4Test to use FastBoardTestCase

--- FastBoard/test/fastboard/checkmove/fastcheck/dcolumn/FastCheckD4Test.java
+++ FastBoard/test/fastboard/checkmove/fastcheck/dcolumn/FastCheckD4Test.java
@@ -1,6 +1,6 @@
 package fastboard.checkmove.fastcheck.dcolumn;
 
-import base.testcase.OthelloTestCase;
+import fastboard.FastBoardTestCase;
 import fastboard.checkmove.calc.FastCheckCalc;
 import fastboard.fastflip.FastBoardFlips;
 import fastboard.lineconverter.LineConverter;
@@ -12,10 +12,10 @@ import fastboard.lineconverter.LineConverter;
  * Time: 02:53:25 PM
  * Tests whether or not FastCheckC4 checks for valid moves properly
  */
-public class FastCheckD4Test extends OthelloTestCase {
+public class FastCheckD4Test extends FastBoardTestCase {
     public void testIsMoveValidBlack() {
         FastCheckCalc calc = new FastCheckCalc();
-        boolean[][] fastCheckCalcArray = calc.calcIsMoveValidForBlack();
+        boolean[][] fastCheckCalcArray = calc.calcIsMoveValid(blackLineDecoders);
         FastCheckD4 check = new FastCheckD4(fastCheckCalcArray);
 
         FastBoardFlips flips = new FastBoardFlips();
@@ -56,7 +56,7 @@ public class FastCheckD4Test extends OthelloTestCase {
 
     public void testIsMoveValidWhite() {
         FastCheckCalc calc = new FastCheckCalc();
-        boolean[][] fastCheckCalcArray = calc.calcIsMoveValidForWhite();
+        boolean[][] fastCheckCalcArray = calc.calcIsMoveValid(whiteLineDecoders);
         FastCheckD4 check = new FastCheckD4(fastCheckCalcArray);
 
         FastBoardFlips flips = new FastBoardFlips();
