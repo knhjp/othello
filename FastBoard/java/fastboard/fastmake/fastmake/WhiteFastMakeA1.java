c8525b70986467be01420bfa5ada51f667095d15
ed <knhjp@oldcomp-desktop.(none)>
2010-03-01 19:40:04 -0600

    Introducing WhiteFastMakeA1

---
+++ FastBoard/java/fastboard/fastmake/fastmake/WhiteFastMakeA1.java
@@ -0,0 +1,11 @@
+package fastboard.fastmake.fastmake;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Mar 1, 2010
+ * Time: 7:39:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WhiteFastMakeA1 {
+}
